c4991e4c34d202ed99eeafffb0d7b43d0a492763
Jose Adrian <joseadria_v12@hotmail.com>
2025-12-01 12:31:50 -0600

    Feat sidebar new sections and adding font awesome

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+         <!-- Fontawesome -->
+
+
+        <script src="https://kit.fontawesome.com/85420b1c77.js" crossorigin="anonymous"></script>
+
+         <!-- Sweetalert2 -->
+
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased">
+
+
+ @include('layouts.includes.admin.navigation')
+ @include('layouts.includes.admin.sidebar') 
+        
+<div class="p-4 sm:ml-64">
+   
+</div>
+
+        @stack('modals')
+
+        @livewireScripts
+
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+
+    </body>
+</html>
